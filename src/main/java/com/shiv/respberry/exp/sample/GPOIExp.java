bec9e12bec94351df365415927b2622437378dce
Shivakumara Narayanaswamy <Shivakumara_Narayanaswamy@intuit.com>
2015-01-05 21:37:20 +0530

    -First program

---
+++ src/main/java/com/shiv/respberry/exp/sample/GPOIExp.java
@@ -0,0 +1,14 @@
+package com.shiv.respberry.exp.sample;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+
+/**
+ * Created by sn1 on 12/25/14.
+ */
+public class GPOIExp {
+    public static void main(String[] args) {
+        final GpioController gpio = GpioFactory.getInstance();
+
+    }
+}
