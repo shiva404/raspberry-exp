da1890e32ed1ca357026163642a3652776c0299a
Shivakumara Narayanaswamy <Shivakumara_Narayanaswamy@intuit.com>
2015-01-07 11:48:24 +0530

    -GPIO listener code

---
+++ src/main/java/com/shiv/respberry/exp/sample/ListenGpioExample.java
@@ -0,0 +1,41 @@
+package com.shiv.respberry.exp.sample;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+/**
+ * Created by sn1 on 1/7/15.
+ */
+public class ListenGpioExample {
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+        // create and register gpio pin listener
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+
+        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+
+        // keep program running until user aborts (CTRL-C)
+        for (;;) {
+            Thread.sleep(500);
+        }
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
+    }
+}
